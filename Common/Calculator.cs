a704cfbd0332f28e2a03f544406f9f4a1207151e
zawmyohtet2009 <zawmyohtet2009@gmail.com>
2025-06-21 21:29:14 +0800

    Create a new common library and nuget package.

---
+++ Common/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using Common.Interface;
+
+namespace Common
+{
+    public class Calculator : ICalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Common/Interface/ICalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Interface
+{
+    interface ICalculator
+    {
+        int Add(int a, int b);
+        int Subtract(int a, int b);
+        int Multiply(int a, int b);
+        int Divide(int a, int b);
+    }
+}
